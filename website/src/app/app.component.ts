86bc222e6ef068ed8cf0d8327db0802840cdb558
lanan <lvu1@smail.uni-koeln.de>
2024-04-25 18:33:54 +0200

    responsiv cookies

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CookieBannerComponent } from './cookie-banner/cookie-banner.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CookieBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
