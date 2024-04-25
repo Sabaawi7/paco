9162aa615d54126cd17454652851b0e4089e5b48
Sabaawi7 <msabaawi@smail.uni-koeln.de>
2024-04-25 08:47:44 +0200

    basic design for landing page

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent} from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
