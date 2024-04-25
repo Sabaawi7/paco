e7f71cda94ced509a09c14d8c3d63661e80c25c1
lanan <lvu1@smail.uni-koeln.de>
2024-04-25 10:36:36 +0200

    Inhalt noch unstrukturiert hinzugefügt

---
+++ website/src/app/login-page/login-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+
+  passwordFieldType: string = 'password'; // Standardwert: Passwort versteckt
+ 
+
+  togglePasswordVisibility() {
+    this.passwordFieldType = (this.passwordFieldType === 'password') ? 'text' : 'password';
+
+  }
+    
+  }
+
+
