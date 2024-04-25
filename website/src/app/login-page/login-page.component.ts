ef30c4631a5125c0c577bd3e95d5986de8389d74
lanan <lvu1@smail.uni-koeln.de>
2024-04-25 17:00:26 +0200

    Inhalt vollständig von login

--- website/src/app/login-page/login-page.component.ts
+++ website/src/app/login-page/login-page.component.ts
@@ -1,21 +1,55 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
 
+
+export class LoginPageComponent {
   passwordFieldType: string = 'password'; // Standardwert: Passwort versteckt
- 
+  email: string = ''; // Variable für E-Mail
+  password: string = ''; // Variable für Passwort
+
+
 
   togglePasswordVisibility() {
     this.passwordFieldType = (this.passwordFieldType === 'password') ? 'text' : 'password';
-
   }
+
+  checkInput() {
+    // Überprüfe, ob E-Mail und Passwort mindestens ein Zeichen enthalten
+    if (this.email.length > 0) {
+      this.setFieldBackgroundColor('emailInput', '#b4f152');
+    } else {
+      this.setFieldBackgroundColor('emailInput', '');
+    }
+
+    if (this.password.length > 0) {
+      this.setFieldBackgroundColor('passwordField', '#b4f152');
+    } else {
+      this.setFieldBackgroundColor('passwordField', '');
+    }
+  }
+
+  // Methode zur Änderung der Hintergrundfarbe eines Feldes
+  private setFieldBackgroundColor(fieldId: string, color: string) {
+    const field = document.getElementById(fieldId);
+    if (field) {
+      field.style.backgroundColor = color;
+    }
+  }
+
+
+ 
+
+
+
+
     
   }
 
