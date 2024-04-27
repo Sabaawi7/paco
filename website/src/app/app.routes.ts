da91e9a634379610aa5fbee611336d1612bd83e1
lanan <lvu1@smail.uni-koeln.de>
2024-04-27 15:12:17 +0200

    Superadmin und Instadmin haben unterschiedlichen Pfad

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { SuperadminComponent } from './superadmin/superadmin.component';
 import { InstadminComponent } from './instadmin/instadmin.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent }, //bestimmt welche Komponente angezeigt wird als Standard wenn man die Webseite als erstes startet;
+  { path: '', component: AppComponent}, //bestimmt welche Komponente angezeigt wird als Standard wenn man die Webseite als erstes startet;
   { path: 'paco/role.com', component: RollenauswahlComponent }, // Rollenauswahlseite, Puffer um zu testen;
   { path: 'paco/superadmin.com', component: SuperadminComponent, data: { role: 'SuperAdmin' } }, // SuperAdmin-Komponente für /paco/admin.com
   { path: 'paco/instadmin.com', component: InstadminComponent, data: { role: 'InstitutionsAdmin' } } // InstitutionsAdmin-Komponente für /paco/admin.com
