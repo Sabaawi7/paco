8ea6231be71a46f1e6dccee467c5727059c3fc75
Sabaawi7 <msabaawi@smail.uni-koeln.de>
2024-04-28 15:18:10 +0200

    Landing Page mit Navigationsbar angepasst und 2 Buttons in der Mitte hinzugefügt

--- website/src/app/landing-page/landing-page.component.ts
+++ website/src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavigationsbarComponent } from "../navigationsbar/navigationsbar.component";
 
 @Component({
-  selector: 'app-landing-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+    selector: 'app-landing-page',
+    standalone: true,
+    templateUrl: './landing-page.component.html',
+    styleUrl: './landing-page.component.scss',
+    imports: [NavigationsbarComponent]
 })
 export class LandingPageComponent {
 
