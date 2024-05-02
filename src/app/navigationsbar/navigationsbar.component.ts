4e233e16e0619c83cdf01273363621362fbe2c6b
Sabaawi7 <msabaawi@smail.uni-koeln.de>
2024-05-02 20:56:05 +0200

    added hamburger menu to the navbar

--- src/app/navigationsbar/navigationsbar.component.ts
+++ src/app/navigationsbar/navigationsbar.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-navigationsbar',
   standalone: true,
   imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './navigationsbar.component.html',
-  styleUrl: './navigationsbar.component.scss'
+  styleUrls: ['./navigationsbar.component.scss']
 })
 export class NavigationsbarComponent {
+  isMenuOpen: boolean = false;
 
+  // Access the navbar-menu DOM element
+  @ViewChild('navbarMenu', { static: false }) navbarMenu!: ElementRef;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    // Adjust the max height of the navbar menu based on its content
+    if (this.isMenuOpen) {
+      // Set the max height to the scroll height of the navbar menu element
+      this.navbarMenu.nativeElement.style.maxHeight = `${this.navbarMenu.nativeElement.scrollHeight}px`;
+    } else {
+      // Collapse the navbar menu by setting max height to 0
+      this.navbarMenu.nativeElement.style.maxHeight = '0';
+    }
+  }
 }
