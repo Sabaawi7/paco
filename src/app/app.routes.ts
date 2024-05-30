ae8e4c16b3bb9a30c60d0e75623132eec721c5b6
lanan <lvu1@smail.uni-koeln.de>
2024-05-30 20:38:38 +0200

    Seite erstellt wo Dashboard angezeigt werden soll

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,9 +8,9 @@ import { ChatbotPageComponent } from './chatbot-page/chatbot-page.component';
 import { ConsultantPageComponent } from './consultant-page/consultant-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { ImpressumPageComponent } from './impressum-page/impressum-page.component';
-
+import { ResultPageComponent } from './result-page/result-page.component';
 export const routes: Routes = [
-    { path: '', component: LandingPageComponent},
+    { path: '', component: ResultPageComponent},
     { path: 'login', component: LoginPageComponent },
     { path: 'role', component: RoleselectionComponent}, // Rollenauswahlseite, Puffer um zu testen;
     { path: 'admin', component: AdminPageComponent }, // Pfad für die Admin-Komponente bleibt unverändert, zeigt entweder Superadmin oder Inst. Admin Seite;
---
+++ src/app/result-page/result-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
+@Component({
+  selector: 'app-result-page',
+  standalone: true,
+  imports: [NavigationBarComponent],
+  templateUrl: './result-page.component.html',
+  styleUrl: './result-page.component.scss'
+})
+export class ResultPageComponent {
+
+}
