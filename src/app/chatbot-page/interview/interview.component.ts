a6eba0a5f83b4c12c6fe350e143099f364ea5c6e
Philipp Brenner <ph.brenner@unitybox.de>
2024-05-31 12:44:41 +0200

    Fehler behoben

--- src/app/chatbot-page/interview/interview.component.ts
+++ src/app/chatbot-page/interview/interview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import interviewJson from '../../../assets/interview.json';
 import { Question } from './question.model';
 import { MaterialModule } from '../../material/material.module';
@@ -16,36 +16,71 @@ import { FormsModule } from '@angular/forms';
 })
 export class InterviewComponent implements OnInit {
 
-  @Input() questions: Question[] = [];
-  @Input() selectedQuestionIndex: number = 0;
-  currentQuestion: Question | null = null;
-  TextInput: string = '';
-  selected: string | number | null = null;
+  @ViewChild('userSelection', { static: false }) userSelection: any;
+  selectedQuestion: number = 1; // Assuming the default selected question is 1
+  totalQuestions: number = this.interviewService.questions.length;
+  selected: any;
+  TextInput: String = '';
+  
   constructor(private interviewService: InterviewService) { }
 
   ngOnInit(): void {
-    this.questions = interviewJson;
-    this.currentQuestion = this.getQuestion(this.selectedQuestionIndex);
+    // Subscribe to selectedQuestion$ to get notified of changes
+    this.interviewService.selectedQuestion$.subscribe(questionNumber => {
+      this.selectedQuestion = questionNumber;
+    });
   }
 
-  getQuestion(index: number): Question | null {
-    return this.questions[index] || null;
+  selectQuestion(questionNumber: number) {
+    this.interviewService.selectQuestion(questionNumber);
   }
 
-  navigateToQuestion(index: number): void {
-    this.selectedQuestionIndex = index;
-    this.currentQuestion = this.getQuestion(index);
+  isQuestionSelected(questionNumber: number): boolean {
+    return this.selectedQuestion === questionNumber;
   }
 
-  navigateToPreviousQuestion(): void {
-    if (this.selectedQuestionIndex > 0) {
-      this.navigateToQuestion(this.selectedQuestionIndex - 1);
+  navigateToNextQuestion(userSelection: MatButtonToggleGroup | undefined) {
+    const currentValue = this.userSelection?.value;
+    console.log(this.TextInput)
+    if(this.userSelection !== undefined && this.userSelection.name.startsWith('mat-button-toggle-group') ){
+      if (currentValue !== undefined && currentValue.length > 0) {
+        this.interviewService.markQuestionAsAnswered(this.selectedQuestion);
+      } 
+
+    } else if (this.selected !== undefined){
+      this.interviewService.markQuestionAsAnswered(this.selectedQuestion);
+    } else if (this.TextInput !== '') {
+      this.interviewService.markQuestionAsAnswered(this.selectedQuestion);
+    } else {
+      console.log(this.userSelection)
+    }
+
+
+    if (this.selectedQuestion < this.totalQuestions -1) {
+      this.selectQuestion(this.selectedQuestion + 1);
+    }
+
+    this.selected = undefined;
+    this.TextInput = '';
+
+    
+  }
+
+  navigateToPreviousQuestion() {
+    if (this.selectedQuestion > 0) {
+      this.selectQuestion(this.selectedQuestion - 1);
     }
   }
 
-  navigateToNextQuestion(): void {
-    if (this.selectedQuestionIndex < this.questions.length - 1) {
-      this.navigateToQuestion(this.selectedQuestionIndex + 1);
+  getQuestion(questionNumber: number): Question {
+    return this.interviewService.getQuestion(questionNumber);
+  }
+
+  getAnswersArray(answers: string[] | number[]): (string | number)[] {
+    if (Array.isArray(answers)) {
+      return answers;
     }
+    return [];
   }
 }
+
