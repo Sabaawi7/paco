5cd7079e765f3eacc181263ea2c84e91ca473ce1
GeorgiosK78 <georgios.kesopoulos02@gmail.com>
2024-06-03 13:54:57 +0200

    response overview komponente mit den antworten und fragen

--- src/app/result-page/response-overview/response-overview.component.ts
+++ src/app/result-page/response-overview/response-overview.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor,NgIf } from '@angular/common';
+import { AnswersService } from '../../chatbot-page/interview/answers.service';
+import { InterviewService } from '../../chatbot-page/interview-service.service';
+import { CommonModule } from '@angular/common';
+import { Question } from '../../chatbot-page/interview/question.model';
 
 @Component({
   selector: 'app-response-overview',
   standalone: true,
-  imports: [],
+  imports: [NgFor,NgIf,CommonModule],
   templateUrl: './response-overview.component.html',
   styleUrl: './response-overview.component.scss'
 })
-export class ResponseOverviewComponent {
+export class ResponseOverviewComponent implements OnInit{
 
+
+  constructor(private answersService: AnswersService, private interviewService: InterviewService) { }
+
+  totalQuestions: number = this.interviewService.questions.length;
+  questions: string[] = [];
+  selectedIndex = 0; 
+  currentQuestion: string = this.questions[this.selectedIndex]; 
+  currentAnswerType: string= this.interviewService.getQuestion(this.selectedIndex).answer_type;
+  currentSelectedAnswer: string = this.answersService.getAnswers(this.selectedIndex);
+  currentAnswer: any[] = [];
+  
+  
+ngOnInit(): void {
+  for(let i=0; i < this.totalQuestions; i++){
+    this.questions.push(this.interviewService.getQuestion(i).question);
+  }
+  this.currentQuestion= this.questions[this.selectedIndex];
+  this.currentAnswerType= this.interviewService.getQuestion(this.selectedIndex).answer_type;
+  this.currentSelectedAnswer= this.answersService.getAnswers(this.selectedIndex); 
+  this.currentAnswer = this.getAnswersArray(this.getQuestion(this.selectedIndex).answers || []);
+}
+
+showDetails(question: number): void {
+  this.currentAnswer = this.getAnswersArray(this.getQuestion(question).answers || []);
+}
+
+selectButton(index: number): void {
+  this.selectedIndex = index;
+}
+
+
+
+
+
+
+
+//Für den Zugriff auf alle Antworten
+getAnswersArray(answers: string[] | number[]): (string | number)[] {
+  if (Array.isArray(answers)) {
+    return answers;
+  }
+  return [];
+}
+getQuestion(questionNumber: number): Question {
+  return this.interviewService.getQuestion(questionNumber);
+}
 }
--- src/app/result-page/result-page.component.ts
+++ src/app/result-page/result-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
 import { UserCredentialsComponent } from './user-credentials/user-credentials.component';
 import { GraphicalAnalysisComponent } from './graphical-analysis/graphical-analysis.component';
+import { ResponseOverviewComponent } from './response-overview/response-overview.component';
 
 @Component({
   selector: 'app-result-page',
   standalone: true,
-  imports: [NavigationBarComponent, UserCredentialsComponent, GraphicalAnalysisComponent],
+  imports: [NavigationBarComponent, UserCredentialsComponent, GraphicalAnalysisComponent, ResponseOverviewComponent],
   templateUrl: './result-page.component.html',
   styleUrl: './result-page.component.scss'
 })
