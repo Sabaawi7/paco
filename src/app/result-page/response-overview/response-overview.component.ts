02507b4c25c006d8edeb2dece6e1c0ba96015d82
GeorgiosK78 <georgios.kesopoulos02@gmail.com>
2024-06-03 11:18:07 +0200

    ersponse overview komponente erstellt

---
+++ src/app/result-page/response-overview/response-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-response-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './response-overview.component.html',
+  styleUrl: './response-overview.component.scss'
+})
+export class ResponseOverviewComponent {
+
+}
