a3396c6b0bb3afa4254e72d57d24488787158b8f
lanan <lvu1@smail.uni-koeln.de>
2024-06-05 11:09:27 +0200

    letzte Komponente hinzugefuegt

--- src/app/result-page/result-page.component.ts
+++ src/app/result-page/result-page.component.ts
@@ -3,11 +3,11 @@ import { NavigationBarComponent } from '../navigation-bar/navigation-bar.compone
 import { UserCredentialsComponent } from './user-credentials/user-credentials.component';
 import { ResponseOverviewComponent } from './response-overview/response-overview.component';
 import { DateAnalysisComponent } from './date-analysis/date-analysis.component';
-
+import { SupportInformationComponent } from './support-information/support-information.component';
 @Component({
   selector: 'app-result-page',
   standalone: true,
-  imports: [NavigationBarComponent, UserCredentialsComponent,ResponseOverviewComponent, DateAnalysisComponent],
+  imports: [NavigationBarComponent, UserCredentialsComponent,ResponseOverviewComponent, DateAnalysisComponent,SupportInformationComponent],
   templateUrl: './result-page.component.html',
   styleUrl: './result-page.component.scss'
 })
