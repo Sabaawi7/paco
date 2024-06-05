dbb48de734c22fffb61f26708c62bb1c46d24703
GeorgiosK78 <georgios.kesopoulos02@gmail.com>
2024-06-05 10:51:51 +0200

    support information komponente fertig

--- src/app/result-page/support-information/support-information.component.ts
+++ src/app/result-page/support-information/support-information.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NgFor,NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-support-information',
   standalone: true,
-  imports: [],
+  imports: [NgFor,NgIf],
   templateUrl: './support-information.component.html',
   styleUrl: './support-information.component.scss'
 })
 export class SupportInformationComponent {
-
+  features=[];
 }
